4803b058bd984c792ecdae390d7f0e969a92d1aa
art-kslv <145579640+art-kslv@users.noreply.github.com>
2025-05-14 08:05:20 -0400

    Design

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,15 @@
 class Category < ApplicationRecord
+  PREDEFINED_CATEGORIES = [
+    "Food",
+    "Travel",
+    "Education",
+    "Health",
+    "Technology"
+  ].freeze
+
+  def self.predefined
+    PREDEFINED_CATEGORIES
+  end
   has_many :tasks, dependent: :nullify
   validates :name, presence: true, uniqueness: true
 end
